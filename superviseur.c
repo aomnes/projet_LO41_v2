ebe3ada6a0764e6e4a8d3e4e39a6b4b7bd01b9fb
aomnes <antoine.omnes@utbm.fr>
2017-01-02 16:03:01 +0100

    ajoute def_in superviseur

--- superviseur.c
+++ superviseur.c
@@ -77,7 +77,7 @@ void superviseur(s_piece **piece)
 				{
 					/* premier passage, installation */
 					alarm(20 * RATIO_TEMPS);//peut etre probleme car fonctionne avec sec...
-		//			usleep((1000000 * 20 - 10000) * RATIO_TEMPS * piece[][].defaut);
+					usleep((1000000 * 20 - 10000) * RATIO_TEMPS * piece[i][j].def_in);
 					alarm(0);
 					fprintf(stdout, "Ok ! Piece sur le convoyeur\n");
 				}
