f3d1910dcb5fa492087487a4877cb058afb6e2dd
aomnes <antoine.omnes@utbm.fr>
2017-01-03 12:49:17 +0100

    file de message suite

--- creation_machine.c
+++ creation_machine.c
@@ -3,35 +3,49 @@
 
 #include "header.h"
 
-void			*fonc_thread(void *k)
+void				*fonc_thread(void *k)
 {
-	s_do_thr	*info_thread;
-	int			nb_piece;
+	s_do_thr		*info_thread;
+	s_msg_env_sup	rep;
+	int				valeur_out_msgrcv;
 
 	info_thread = (s_do_thr*)k;
 	printf("Machine %d allumee\n", info_thread->num_thread);
 
-	//do
-	// rep = msgrcv(...)
-	//while(rep != num_mach)
+	do
+	{
+		if (msgrcv(msgid_machine, &rep, sizeof(s_msg_env_sup), 0, 0) == -1)
+			error("msgrcv creation_machine rep #1");
+	} while (info_thread->num_thread != rep.num_machine);
+	//ne pas oublier eteindre la si la machine n a pas de pieces (!nb_piece_type)
+	do
+	{
+		errno = 0;
+		valeur_out_msgrcv = msgrcv(msgid_machine, &rep, sizeof(s_msg_env_sup), 0, IPC_NOWAIT);
+		 if (errno != ENOMSG && valeur_out_msgrcv != 0)
+			error("msgrcv creation_machine vide file message #1");
+	} while (errno != ENOMSG);//vider file de message
 
 	while (1)
 	{
-		if (!nb_piece)//nb_recu par msgrcv();
+		if (!rep.nb_piece_type)//nb_recu par msgrcv(); pb ==> si pas de piece alors on allume pas la machine donc pas de message a envoyer utilisation varaible globale
 			break;
-		//do
-		// rep = msgrcv(...) ordre
-		//vider la file de message au cas ou il reste des message non lu du a la creation de piece 10min
-		// errno = 0;//au plus proche du msgsnd pour eviter tout pb de changement de errno...
-		//do
-		// rep =msgrcv(..., IPC_NOWAIT)
-		// if (rep != ENOMSG && rep != 0) { error("msgrcv creation_machine")}
-		//ou while (errno != ENOMSG)
-		// num_mach = struct.num;
-		//while(rep != num_mach)
-		//alarm(10min);
+		do
+		{
+			if (msgrcv(msgid_machine, &rep, sizeof(s_msg_env_sup), 0, 0) == -1)
+				error("msgrcv creation_machine rep #2");
+		} while (info_thread->num_thread != rep.num_machine);
+		do
+		{
+			errno = 0;
+			valeur_out_msgrcv = msgrcv(msgid_machine, &rep, sizeof(s_msg_env_sup), 0, IPC_NOWAIT);
+			 if (errno != ENOMSG && valeur_out_msgrcv != 0)
+				error("msgrcv creation_machine vide file message #2");
+		} while (errno != ENOMSG);//vider file de message
+
+		alarm(1);
 		//msgsnd(...) compte rendu
-		if (msgsnd(msgid_in, piece, sizeof(s_piece), 0) == -1)
+		if (msgsnd(msgid_in, &rep.piece, sizeof(s_piece), 0) == -1)
 			error("msgsnd msgid_out");
 	}
 	printf("Machine %d eteinte\n", info_thread->num_thread);
